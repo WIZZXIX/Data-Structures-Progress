6cb0ee96cb46c371ff785b6930d351d7da80c2c9
WIZZXIX <suchitmanthen@gmail.com>
2025-07-30 22:22:03 +0530

    Variables and Datatypes in C++

---
+++ C++/02_Variables_Datatypes/01_variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+
+int main() {
+    int num; //Declaration of the variable 
+    num = 15; //Assignment of the variable 
+
+    cout << "Number: " << num; 
+    
+    int sum = 50; //Initialization of the variable 
+    cout << "\nSum: " << sum;
+
+    return 0; 
+}
+
+//variable should always start with the some character or underscore (a-z or _)
