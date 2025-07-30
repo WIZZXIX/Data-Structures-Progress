ea9af8a9035acc1a4b03d716ed926dfebcabceeb
WIZZXIX <suchitmanthen@gmail.com>
2025-07-30 20:13:15 +0530

    Square number

---
+++ C++/01_Basics/square.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+
+int main() {
+    int num; 
+    cout << "Enter the number: "; 
+    cin >> num; 
+
+    int square = num * num;
+    cout << "Square of the number is: " << square << endl;
+}
