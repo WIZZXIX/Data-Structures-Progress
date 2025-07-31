f4f95bce5e15c29ff340af82ff8557a59fce5841
WIZZXIX <suchitmanthen@gmail.com>
2025-07-31 11:52:39 +0530

    Arithmatic operators

---
+++ C++/03_Operators/01_arithmatic.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std; 
+
+int main() {
+    int a, b; 
+    cout << "Enter the first number: "; 
+    cin >> a; 
+
+    cout << "Enter the second number: "; 
+    cin >> b; 
+
+    cout << "a + b = " << a + b << endl; //addition 
+    cout << "a - b = " << a - b << endl; //substraction 
+    cout << "a * b = " << a * b << endl; //multiplication 
+    cout << "a / b = " << a / b << endl; //division 
+    cout << "a % b = " << a % b << endl; //modulus 
+
+    return 0; 
+}
