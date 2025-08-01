e351590d0a7f6300b8b228580aced299e8742458
WIZZXIX <suchitmanthen@gmail.com>
2025-08-01 11:38:38 +0530

    Increment and Decrement in C++

---
+++ C++/03_Operators/03_increment_decrement.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    int num1 = 5; 
+    int num2 = 5; 
+
+    cout << "Orginal values: " << num1 << " " << num2 << endl;
+
+    num1 ++ ; 
+    num2 ++ ; 
+    cout << "Incremented value: " << num1 << " " << num2 << endl;
+
+    num1 --; 
+    num2 --; 
+    cout << "Decremented value: " << num1 << " " << num2 << endl; 
+
+    return 0; 
+}
